3a5bdc6676fd55b91491e4fab64d63fde285034e
Henrique Miranda <henriquepw4@gmail.com>
2026-07-05 12:51:51 -0300

    remove old config

--- .config/nvim/lua/plugins/blink.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({
-	"https://github.com/Saghen/blink.cmp",
-})
-
-require("blink.cmp").setup()
--- .config/nvim/lua/plugins/theme.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.termguicolors = true
-
-vim.pack.add({
-  "https://github.com/ficd0/ashen.nvim"
-})
-
-require("ashen").setup({ transparent = true })
-vim.cmd("colorscheme ashen")
-
