b43185b2bf855e6164e1d2d7b22aba31c0015972
Henrique Miranda <henriquepw4@gmail.com>
2026-08-02 14:38:04 -0300

    setup syncthing dir

--- modules/features/syncthing.nix
+++ modules/features/syncthing.nix
@@ -1,12 +1,22 @@
 { ... }:
 {
   flake.nixosModules.syncthing =
-    { pkgs, ... }:
+    { config, pkgs, user, ... }:
+    let
+      home = config.users.users.${user}.home;
+    in
     {
       services.syncthing = {
+        inherit user;
         enable = true;
         openDefaultPorts = true;
         guiAddress = "0.0.0.0:8384";
+        group = "users";
+        dataDir = home;
+        configDir = "${home}/.config/syncthing";
       };
+
+      # openDefaultPorts only opens sync ports, not the GUI
+      networking.firewall.allowedTCPPorts = [ 8384 ];
     };
 }
